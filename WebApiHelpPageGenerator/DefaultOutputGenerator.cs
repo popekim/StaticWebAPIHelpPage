b848a4b5b747dfc4c07ddd267e3c6b3d3448e465
Pope Kim <popekim@gmail.com>
2016-12-25 11:54:08 -0800

    initial checkin after a small cleanup

---
+++ WebApiHelpPageGenerator/DefaultOutputGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Web.Http.Description;
+using WebApiHelpPage;
+using WebApiHelpPage.Models;
+
+namespace WebApiHelpPageGenerator
+{
+    public class DefaultOutputGenerator : IOutputGenerator
+    {
+        private const string fileName = "HelpPage.html";
+
+        public void GenerateIndex(Collection<ApiDescription> apis)
+        {
+            Index indexTemplate = new Index
+            {
+                Model = apis,
+                ApiLinkFactory = apiName =>
+                {
+                    return apiName + ".html";
+                }
+            };
+            string helpPageIndex = indexTemplate.TransformText();
+            WriteFile(fileName, helpPageIndex);
+        }
+
+        public void GenerateApiDetails(HelpPageApiModel apiModel)
+        {
+            Api apiTemplate = new Api
+            {
+                Model = apiModel,
+                HomePageLink = fileName
+            };
+            string helpPage = apiTemplate.TransformText();
+            WriteFile(apiModel.ApiDescription.GetFriendlyId() + ".html", helpPage);
+        }
+
+        private static void WriteFile(string fileName, String pageContent)
+        {
+            Console.WriteLine("writing file: {0}", fileName);
+            File.WriteAllText(fileName, pageContent);
+        }
+    }
+}
---
+++ WebApiHelpPageGenerator/HttpConfigurationImporter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http;
+
+namespace WebApiHelpPageGenerator
+{
+    public static class HttpConfigurationImporter
+    {
+        public static readonly string ConfigurationMethodName = "Register";
+        public static readonly string ConfigClassName = "WebApiConfig";
+        public static readonly string HelpPageConfigClassName = "HelpPageConfig";
+
+        public static HttpConfiguration ImportConfiguration(string assemblyPath)
+        {
+            var assembly = Assembly.LoadFrom(assemblyPath);
+
+            string originalDirectory = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Path.GetDirectoryName(assemblyPath);
+
+            Type webApiConfigType = assembly.GetTypes().FirstOrDefault(t => t.Name == ConfigClassName);
+            if (webApiConfigType == null)
+            {
+                throw new InvalidOperationException(string.Format("Cannot find the configuration class: '{0}' in {1}", ConfigClassName, assemblyPath));
+            }
+            MethodInfo registerConfigMethod = webApiConfigType.GetMethod(ConfigurationMethodName, BindingFlags.Static | BindingFlags.Public);
+            if (registerConfigMethod == null)
+            {
+                throw new InvalidOperationException(string.Format("Cannot find the static configuration method: '{0}()' in {1}", ConfigurationMethodName, ConfigClassName));
+            }
+
+            Action<HttpConfiguration> registerConfig = Delegate.CreateDelegate(typeof(Action<HttpConfiguration>), registerConfigMethod) as Action<HttpConfiguration>;
+            HttpConfiguration config = new HttpConfiguration();
+            registerConfig(config);
+            ImportHelpPageConfiguration(assembly, config);
+            Environment.CurrentDirectory = originalDirectory;
+            return config;
+        }
+
+        private static HttpConfiguration ImportHelpPageConfiguration(Assembly assembly, HttpConfiguration config)
+        {
+            Type helpPageConfigType = assembly.GetTypes().FirstOrDefault(t => t.Name == HelpPageConfigClassName);
+            if (helpPageConfigType != null)
+            {
+                MethodInfo registerConfigMethod = helpPageConfigType.GetMethod(ConfigurationMethodName, BindingFlags.Static | BindingFlags.Public);
+                if (registerConfigMethod == null)
+                {
+                    throw new InvalidOperationException(string.Format("Cannot find the static configuration method: '{0}()' in {1}", ConfigurationMethodName, HelpPageConfigClassName));
+                }
+
+                Action<HttpConfiguration> registerConfig = Delegate.CreateDelegate(typeof(Action<HttpConfiguration>), registerConfigMethod) as Action<HttpConfiguration>;
+                registerConfig(config);
+            }
+
+            return config;
+        }
+    }
+}
